ae82dab482675c12bae4a2f672e5c2e8931207e7
Joeri Bakker <joeri@bunq.com>
2017-01-10 18:10:42 +0100

    Added HEX validation (#1)
    Signed-off-by: Joeri Bakker <joeri@bunq.com>

--- src/SwatchColor.js
+++ src/SwatchColor.js
@@ -3,12 +3,41 @@ define(function () {
 		patternValidHexValue = /^#[0-9A-F]{6}$/i,
 		isValidHexValue;
 
+	/**
+	 * Construct a new SwatchColor.
+	 * 
+	 * @param	{string}	hexValue 	A valid HEX color value.
+	 */
 	function SwatchColor (hexValue) {
-		this.hexValue = hexValue;
-	}
+		this.setHexValue(hexValue);
+	};
 
+	/**
+	 * @returns	{string}
+	 */
 	SwatchColor.prototype.getHexValue = function () {
 		return this.hexValue;
+	};
+
+	/**
+	 * @param	{string}	hexValue 	A valid HEX color value.
+	 */
+	SwatchColor.prototype.setHexValue = function (hexValue) {
+		if (isValidHexValue(hexValue)) {
+			this.hexValue = hexValue;
+		} else if (!this.hexValue) {
+			this.hexValue = defaultHexValue;
+		}
+	};
+
+	/**
+	 * Check if a given value is a string containing a valid HEX color value.
+	 *
+	 * @param	{string}	hexValue 	Any string to test the validity of.
+	 */
+	isValidHexValue = function (hexValue) {
+		return typeof hexValue === "string" 
+					&& patternValidHexValue.test(hexValue);
 	};
 
 	return SwatchColor;
